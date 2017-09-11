6f66b418b9f5923e692fa48f8d35dcccff6b5ce6
Johannes Brodwall <johannes@brodwall.com>
2017-09-11 22:44:10 +0200

    Refactor

--- src/main/java/com/soprasteria/javazone/infrastructure/server/AbstractSyncServer.java
+++ src/main/java/com/soprasteria/javazone/infrastructure/server/AbstractSyncServer.java
@@ -2,13 +2,20 @@ package com.soprasteria.javazone.infrastructure.server;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import org.jsonbuddy.JsonArray;
+
+import com.soprasteria.javazone.infrastructure.ExceptionHelper;
 
 @SuppressWarnings("restriction")
 public abstract class AbstractSyncServer {
@@ -47,6 +54,24 @@ public abstract class AbstractSyncServer {
                 output.write(buffer);
             }
         });
+    }
+
+    protected Map<String, String> parseParameters(String query) {
+        HashMap<String, String> parameters = new HashMap<>();
+        for (String parameterString : query.split("&")) {
+            int parts = parameterString.indexOf('=');
+            parameters.put(urlDecode(parameterString.substring(0, parts)),
+                parts > 0 ? urlDecode(parameterString.substring(parts+1)) : "");
+        }
+        return parameters;
+    }
+
+    private String urlDecode(String s) {
+        try {
+            return URLDecoder.decode(s, StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException e) {
+            throw ExceptionHelper.soften(e);
+        }
     }
 
 }
--- src/main/java/com/soprasteria/javazone/person/PersonSyncServer.java
+++ src/main/java/com/soprasteria/javazone/person/PersonSyncServer.java
@@ -1,16 +1,11 @@
 package com.soprasteria.javazone.person;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.jsonbuddy.JsonArray;
 
-import com.soprasteria.javazone.infrastructure.ExceptionHelper;
 import com.soprasteria.javazone.infrastructure.server.AbstractSyncServer;
 
 public class PersonSyncServer extends AbstractSyncServer {
@@ -28,24 +23,6 @@ public class PersonSyncServer extends AbstractSyncServer {
             Instant since = Instant.parse(parameters.get("since"));
             return JsonArray.map(repository.listChanges(since), Person::toJson);
         });
-    }
-
-    private Map<String, String> parseParameters(String query) {
-        HashMap<String, String> parameters = new HashMap<>();
-        for (String parameterString : query.split("&")) {
-            int parts = parameterString.indexOf('=');
-            parameters.put(urlDecode(parameterString.substring(0, parts)),
-                parts > 0 ? urlDecode(parameterString.substring(parts+1)) : "");
-        }
-        return parameters;
-    }
-
-    private String urlDecode(String s) {
-        try {
-            return URLDecoder.decode(s, StandardCharsets.UTF_8.name());
-        } catch (UnsupportedEncodingException e) {
-            throw ExceptionHelper.soften(e);
-        }
     }
 
 }
