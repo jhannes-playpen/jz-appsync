97dae03a35cbf3077a5be0b2f9924aaf32b7d5c4
Johannes Brodwall <johannes@brodwall.com>
2017-09-11 21:01:37 +0200

    Add all fields to sampleData generator

--- src/test/java/com/soprasteria/javazone/person/PersonRepositoryTest.java
+++ src/test/java/com/soprasteria/javazone/person/PersonRepositoryTest.java
@@ -2,6 +2,7 @@ package com.soprasteria.javazone.person;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
 import java.util.Random;
 
 import javax.sql.DataSource;
@@ -30,9 +31,18 @@ public class PersonRepositoryTest {
     }
 
     private Person samplePerson() {
+        String[] lastNames = new String[] { "Brodwall", "Adams", "Smith", "Jones" };
+
         Person person = new Person();
         person.setFirstName(pickOne(new String[] { "Johannes", "John", "James", "Peter", "Paul" }));
+        person.setMiddleName(pickOne(lastNames));
+        person.setLastName(pickOne(lastNames));
+        person.setDateOfBirth(randomBirthDate());
         return person;
+    }
+
+    private LocalDate randomBirthDate() {
+        return LocalDate.now().minusYears(20).minusDays(random.nextInt(365*30));
     }
 
     private <T> T pickOne(T[] strings) {
